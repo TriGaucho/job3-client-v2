6cb1ec339314ea154d4c4a636387b8246ec8e5ef
matheuzmelo <matheusprestesdmelo744@gmail.com>
2025-02-06 13:45:47 -0300

    feat: config de deploy no vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/job3-front-end'
+  base: '/job3-client-v2'
 })
