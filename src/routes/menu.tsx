6438c1a85d410b7420f9834f1f50adeea06171d3
matheuzmelo <matheusprestesdmelo744@gmail.com>
2025-02-06 14:06:14 -0300

    feat: config de deploy no vite

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -11,47 +11,48 @@ import Proposta from '../components/pages/Proposal/Index';
 import { MenuLayout } from '../types/TMenu.type';
 import Pessoas from '../components/pages/Pessoas';
 import GroupIcon from '@mui/icons-material/Group';
+import { GENERAL_PATH } from '../Utils/constants';
 
 
 export const menuLayout: MenuLayout = [
     {
         name: "Home",
-        route: "/",
+        route: `${GENERAL_PATH}/`,
         page: <Home />,
         icon: <HomeRoundedIcon />,
         permission: undefined,
     },
     {
         name: "Proposta",
-        route: "/proposta",
+        route: `${GENERAL_PATH}/proposta`,
         page: <Proposta />,
         icon: <NoteAltRoundedIcon />,
         permission: 'PROPOSTAS',
     },
     {
         name: "Clientes",
-        route: "/clientes",
+        route: `${GENERAL_PATH}/clientes`,
         page: <Clientes />,
         icon: <GroupRoundedIcon />,
         permission: undefined,
     },
     {
         name: "Produtos",
-        route: "/produtos",
+        route: `${GENERAL_PATH}/produtos`,
         page: <Produtos />,
         icon: <InventoryIcon />,
         permission: undefined,
     },
     {
         name: "Notas Fiscais",
-        route: "/notas-fiscais",
+        route: `${GENERAL_PATH}/notas-fiscais`,
         page: <Nfe />,
         icon: <LocalAtmRounded />,
         permission: 'NOTAS_FISCAIS',
     },
     {
         name: "Pessoas",
-        route: "/pessoas",
+        route: `${GENERAL_PATH}/pessoas`,
         page: <Pessoas />,
         icon: <GroupIcon />,
         permission: undefined
