1580a96579afd0870b6186068455eeec8402f7d4
matheuzmelo <matheusprestesdmelo744@gmail.com>
2025-02-11 20:39:13 -0300

    feat: add permission for access site

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      'technological-kiwi-mmcode-27e018c9.koyeb.app', // Adicione o domínio do Koyeb aqui
+    ],
+  },
+});
