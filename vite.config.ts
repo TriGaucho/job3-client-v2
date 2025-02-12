79ef159a3cd996aeeaf97eab59f4825ea15b13bc
matheuzmelo <matheusprestesdmelo744@gmail.com>
2025-02-11 23:42:47 -0300

    feat: ajuste nas rotas do menu

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      'technological-kiwi-mmcode-27e018c9.koyeb.app', // Adicione o domínio do Koyeb aqui
-    ],
-  },
 });
